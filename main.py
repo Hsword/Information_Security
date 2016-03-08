42143532836f566de8827325e3c730fbc09f08c9
Hsword <swordonline@foxmail.com>
2016-03-08 22:58:07 +0800

    Add Key controlled seqence encryption

--- main.py
+++ main.py
@@ -1,28 +1,57 @@
 #-*- coding: utf8 -*-
 import os
-def Text_Clean( text ):
+def Text_Clean(text):
 	ans = ""
 	for letter in text:
 		if letter.isalpha():
 			ans = ans + letter
 	return ans
 
-def Show_Ans( text ):
-	print "Ciphertext is:"
-	print text
+def Show_Ans(text):
+	print "Ciphertext is          :"+text
 	return
 
 def Double_Track():
-	plain_text = raw_input ( "Please input plain text:" )
-	plain_text = Text_Clean ( plain_text )
-                ans = ""
-                length = len ( plain_text )
-                for index in [ 0, 1 ]:
-                	while index < length:
-                		ans = ans +plain_text[index]
-                		index = index + 2
-                Show_Ans(ans)
-                return
+	plain_text = raw_input( "Please input plain text:" )
+	plain_text = Text_Clean (plain_text)
+	ans = ""
+	length = len(plain_text)
+	index=0
+	while index<length:
+		ans=ans+plain_text[index]
+		index=index+2
+	index=1
+	while index<length:
+		ans=ans+plain_text[index]
+		index=index+2
+	Show_Ans(ans)
+	return
+
+def my_cmp(x,y):
+	return cmp(x[0],y[0])
+
+def Key_Controller_Sequence():
+	plain_text = raw_input("Please input plain text:")
+	plain_text=Text_Clean(plain_text)
+	key = raw_input("Please input key:")
+	key=Text_Clean(key)
+	plength=len(plain_text)
+	klength=len(key)
+	ans=""
+	if plength>=klength:
+		mod=plength%klength
+		plength=plength-mod
+		mat=[]
+		for index in xrange(0,klength):
+			mat.append((key[index],[]))
+		for index in xrange(0,plength):
+			mat[index%klength][1].append(plain_text[index])
+		mat.sort(my_cmp)
+		for index in xrange(0,plength/klength):
+			for k in xrange(0,klength):
+				ans=ans+mat[k][1][index]
+	Show_Ans(ans)
+	return
 
 
 num_selections = 2
@@ -37,7 +66,7 @@ while True:
 		continue
 	else:
 		break
-if num == 1:
+if num==1:
 	Double_Track()
-else if num == 2:
-	exit()
+elif num==2:
+	Key_Controller_Sequence()
